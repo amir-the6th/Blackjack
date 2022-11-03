242a6c70529726f090355c61efa0ed331d40944b
Amir Sabagh <amir.sabagh80@gmail.com>
2022-11-03 17:13:43 -0400

    Create a function to generate random card numbers

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+// Make this function return a random number between 1 and 13
+function getRandomCard() {
+    return Math.floor(Math.random() * 13) + 1;
+}
 
 function startGame() {
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: "
+    cardsEl.textContent = "Cards: ";
     // Create a for loop that renders out all the cards instead of just two
     for(let i=0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " ";
     }
     
-    sumEl.textContent = "Sum: " + sum
+    sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
-        message = "Do you want to draw a new card?"
+        message = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
     } else {
-        message = "You're out of the game!"
-        isAlive = false
+        message = "You're out of the game!";
+        isAlive = false;
     }
-    messageEl.textContent = message
+    messageEl.textContent = message;
 }
 
 
 function newCard() {
-    let card = 6
-    sum += card
+    let card = getRandomCard();
+    sum += card;
     // Push the card to the cards array
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+    cards.push(card);
+    renderGame();
 }
